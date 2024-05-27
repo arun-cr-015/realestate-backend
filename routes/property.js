47b093d40ccd7bdc6dfa1421729c2a6c68378776
arun <arundgj2000@gmail.com>
2024-05-27 16:37:04 +0530

    mail done

--- routes/property.js
+++ routes/property.js
@@ -7,14 +7,14 @@ const { auth } = require('./user');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey('YOUR_SENDGRID_API_KEY');
 const Mailjet = require('node-mailjet');
-// Setup nodemailer transporter
-// const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: '@gmail.com',
-//         pass: ''
-//     }
-// });
+
+const transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+        user: 'rentifyarun@hotmail.com',
+        pass: 'Rentify@2001'
+    }
+});
 
 // const mailjet = Mailjet.apiConnect(
 //     '81b3aa4ad884e51f65da9d0093e3c258',
@@ -81,20 +81,59 @@ router.post('/:id/like', async (req, res) => {
     }
 });
 
-router.post('/interest/:propertyId',  async (req, res) => {
+router.post('/interest/:propertyId/:userId',  async (req, res) => {
     try {
         const property = await Property.findById(req.params.propertyId);
         if (!property) {
             return res.status(404).json({ error: 'Property not found' });
         }
-        console.log(property)
+       
         const seller = await User.findById(property.postedBy);
-        const buyer = await User.findById(req.userId);
+        const buyer = await User.findById(req.params.userId);
 
         if (!seller) {
             return res.status(404).json({ error: 'User not found' });
         }
-    
+        if (!buyer) {
+            return res.status(404).json({ error: 'Buyer not found' });
+        }
+
+       
+        const buyOpt = {
+            from: "rentifyarun@hotmail.com",
+            to: buyer.email,
+            subject: "Interest in Property",
+            text: `Dear ${buyer.firstName},\n\nYou have shown interest in the property with Name: ${property.title}.\nContact Details\nEmail:${seller.email}\nPhonenumber:${seller.phoneNumber}\n\nThank you,\nRentify Team`,
+        };
+
+     
+        const sellOpt = {
+            from: "rentifyarun@hotmail.com",
+            to: seller.email,
+            subject: "Property Interest Notification",
+            text: `Dear ${seller.firstName},\n\nA buyer has shown interest in your property with Name: ${property._id}.
+            \nContact Details\nEmail:${buyer.email}\nPhonenumber:${buyer.phoneNumber}
+            \n\nThank you,\nRentify Team`,
+        };
+
+        
+        transporter.sendMail(buyOpt, (err, info) => {
+            if (err) {
+                console.log('Error sending email to buyer:', err.message);
+            } else {
+                console.log('Email sent to buyer:', info.response);
+            }
+        });
+
+        // Send email to the seller
+        transporter.sendMail(sellOpt, (err, info) => {
+            if (err) {
+                console.log('Error sending email to seller:', err.message);
+            } else {
+                console.log('Email sent to seller:', info.response);
+            }
+        });
+
         res.status(200).json({ sellerEmail: seller.email, sellerPhoneNumber:seller.phoneNumber,sellerName:seller.firstName });
     } catch (err) {
         console.error(err);
